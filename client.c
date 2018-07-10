ce351f5d97b57d74ebbfc4457b37409dc639a80f
Vinicius <vinibfranc@gmail.com>
2018-07-10 16:55:57 -0300

    Alterações no cliente

--- client.c
+++ client.c
@@ -8,11 +8,14 @@
 #include <pthread.h>
 #include <time.h>
 
-#define CLIENTS 4
-void *t_join_solicitation;
-void *t_join_receiving;
-pthread_t t_solicitation[CLIENTS];
-pthread_t t_receiving[CLIENTS];
+/* TODO: Alterar para quantidade de hosts (alunos) conforme quantos forem apresentar */
+#define CLIENTS 13
+//void *t_join_solicitation;
+//void *t_join_receiving;
+//pthread_t t_solicitation[CLIENTS];
+//pthread_t t_receiving[CLIENTS];
+void *t_client_join;
+pthread_t t_client[CLIENTS];
 pthread_mutex_t lock;
 
 int rem_sockfd;
@@ -41,92 +44,11 @@ int random_quantity() {
 	return num;
 }
 
-/* Função que será associada a uma thread para realizar múltiplas solicitações */
-int *send_solicitation() {
-	pthread_mutex_lock(&lock);
-	
-	int quantity = random_quantity();
-	
-	/* Inicializando mensagem */
-	aatp_msg m = { 0 };
-	/* Preenchendo dados */
-	m.method = 'S'; /* Solicitação */
-	//m.size = 1;
-	m.size = quantity;
-	/* Zerando payload */
-	memset(&m.payload, 0, sizeof m.payload);
-	/* Enviando solicitação */
-	int r = send(rem_sockfd, &m, sizeof(m), 0);
-	printf("Enviando solicitação!!");
-	pthread_mutex_unlock(&lock);
+void *client_tasks() {
 
-	return r;
-}
-
-/* Função associada a uma thread para realizar recebimentos simultâneos */
-void *receive_aminoacids() {
-	
-	pthread_mutex_lock(&lock);
-
-	/* Criando buffer */
-	aatp_msg recv_buffer;
-	size_t recv_buffer_len = sizeof(recv_buffer);
-	/* Recebendo mensagem */
-	recv(rem_sockfd, &recv_buffer, recv_buffer_len, 0);
-	/* Imprimindo resultados */
-	printf("\t Method: %c\n", recv_buffer.method);
-	printf("\t Size: %d\n", recv_buffer.size);
-	printf("\t Payload: %s\n", recv_buffer.payload);
-
-	pthread_mutex_unlock(&lock);
-}
-
-/* Retorna a lista de IPs presentes no arquivo de configuração */
-int get_ips() {
-	FILE *config_file;
-	char each_line[2][15];
-	int i=0;
-	if(!(fopen("config.txt", "r"))) {
-		printf("Erro ao abrir o arquivo!");
-		return 1;
-	}
-	//pega todos os IPs
-	while(fgets(each_line[i], CLIENTS, config_file) != NULL) {
-		ips[i] = each_line[i];
-		printf("%s", each_line[i]);
-		i++;
-	}
-}
-
-void initialize_threads_solicitation() {
-	int i;
-    	for(i=0; i<CLIENTS; i++) {
-       		pthread_create(&t_solicitation[i], NULL, send_solicitation, NULL);
-   	}
-    	for(i=0; i<CLIENTS; i++) {
-        	pthread_join(t_solicitation[i],&t_join_solicitation);
-	}
-} 
-
-void initialize_threads_receiving() {
-	int i;
-    	for(i=0; i<CLIENTS; i++) {
-		pthread_create(&t_receiving[i], NULL, receive_aminoacids, NULL);
-   	}
-    	for(i=0; i<CLIENTS; i++) {
-		pthread_join(t_receiving[i],&t_join_receiving);
-	}
-} 
-
-
-/*==================================================
-
-Implementação do cliente
-
-====================================================*/
-
-/* Conexão do cliente com servidor e solicitação dos aminoácidos */
-int main(int argc, char *argv[]) {
+	/*========================
+	Implementação do cliente
+	==========================*/
 
 	/* ESTACAO REMOTA */
 	char *rem_hostname;
@@ -136,11 +58,11 @@ int main(int argc, char *argv[]) {
 	struct sockaddr_in rem_addr;
 	char linha[81];
 
-	if (argc != 3) {
+	/*if (argc != 3) {
 		//printf("Parametros: <remote_port> \n");
 		printf("Parametros: <remote_host> <remote_port> \n");
 		exit(1);
-	}
+	}*/
 
 	/* Construcao da estrutura do endereco local */
 	/* Preenchendo a estrutura socket loc_addr (família, IP, porta) */
@@ -148,8 +70,10 @@ int main(int argc, char *argv[]) {
 
 	//rem_hostname = ips[0];
 
-	rem_hostname = argv[1];
-	rem_port = atoi(argv[2]);
+	/*rem_hostname = argv[1];
+	rem_port = atoi(argv[2]);*/
+	rem_hostname = atoi("127.0.0.1");
+	rem_port = atoi("1337");
 	rem_addr.sin_family = AF_INET; /* familia do protocolo*/
 	rem_addr.sin_addr.s_addr = inet_addr(rem_hostname); /* endereco IP local */
 	rem_addr.sin_port = htons(rem_port); /* porta local  */
@@ -170,27 +94,155 @@ int main(int argc, char *argv[]) {
 		perror("Conectando stream socket");
 		exit(1);
 	}
+
+	/*========================
+	Envio da solicitação
+	==========================*/
+
+	int quantity = random_quantity();
 	
-	//initialize_threads();
+	/* Inicializando mensagem */
+	aatp_msg m = { 0 };
+	/* Preenchendo dados */
+	m.method = 'S'; /* Solicitação */
+	//m.size = 1;
+	m.size = quantity;
+	/* Zerando payload */
+	memset(&m.payload, 0, sizeof m.payload);
+	/* Enviando solicitação */
+	send(rem_sockfd, &m, sizeof(m), 0);
+	printf("Enviando solicitação!!");
+
+	/*========================
+	Recebimento da resposta
+	==========================*/
+
+	/* Criando buffer */
+	aatp_msg recv_buffer;
+	size_t recv_buffer_len = sizeof(recv_buffer);
+	/* Recebendo mensagem */
+	recv(rem_sockfd, &recv_buffer, recv_buffer_len, 0);
+	/* Imprimindo resultados */
+	printf("\t Method: %c\n", recv_buffer.method);
+	printf("\t Size: %d\n", recv_buffer.size);
+	printf("\t Payload: %s\n", recv_buffer.payload);
+	
+}
+
+/* Função que será associada a uma thread para realizar múltiplas solicitações
+int *send_solicitation() {
+	pthread_mutex_lock(&lock);
+	
+	int quantity = random_quantity();
+	
+	Inicializando mensagem
+	aatp_msg m = { 0 };
+	Preenchendo dados
+	m.method = 'S'; Solicitação
+	//m.size = 1;
+	m.size = quantity;
+	Zerando payload
+	memset(&m.payload, 0, sizeof m.payload);
+	Enviando solicitação
+	int r = send(rem_sockfd, &m, sizeof(m), 0);
+	printf("Enviando solicitação!!");
+	pthread_mutex_unlock(&lock);
+
+	return r;
+}*/
+
+/* Função associada a uma thread para realizar recebimentos simultâneos
+void *receive_aminoacids() {
+	
+	pthread_mutex_lock(&lock);
+
+	Criando buffer
+	aatp_msg recv_buffer;
+	size_t recv_buffer_len = sizeof(recv_buffer);
+	Recebendo mensagem
+	recv(rem_sockfd, &recv_buffer, recv_buffer_len, 0);
+	Imprimindo resultados
+	printf("\t Method: %c\n", recv_buffer.method);
+	printf("\t Size: %d\n", recv_buffer.size);
+	printf("\t Payload: %s\n", recv_buffer.payload);
+
+	pthread_mutex_unlock(&lock);
+}*/
+
+/* Retorna a lista de IPs presentes no arquivo de configuração */
+int get_ips() {
+	FILE *config_file;
+	char each_line[2][15];
+	int i=0;
+	if(!(fopen("config.txt", "r"))) {
+		printf("Erro ao abrir o arquivo!");
+		return 1;
+	}
+	//pega todos os IPs
+	while(fgets(each_line[i], CLIENTS, config_file) != NULL) {
+		ips[i] = each_line[i];
+		printf("%s", each_line[i]);
+		i++;
+	}
+}
+
+/*void initialize_threads_solicitation() {
+	int i;
+    	for(i=0; i<CLIENTS; i++) {
+       		pthread_create(&t_solicitation[i], NULL, send_solicitation, NULL);
+   	}
+    	for(i=0; i<CLIENTS; i++) {
+        	pthread_join(t_solicitation[i],&t_join_solicitation);
+	}
+}*/ 
+
+/*void initialize_threads_receiving() {
+	int i;
+    	for(i=0; i<CLIENTS; i++) {
+		pthread_create(&t_receiving[i], NULL, receive_aminoacids, NULL);
+   	}
+    	for(i=0; i<CLIENTS; i++) {
+		pthread_join(t_receiving[i],&t_join_receiving);
+	}
+}*/ 
+
+
+/*==================================================
+
+Implementação do cliente
+
+====================================================*/
+
+/* Conexão do cliente com servidor e solicitação dos aminoácidos */
+
+int main(int argc, char *argv[]) {
+
+	int i;
+    	for(i=0; i<CLIENTS; i++) {
+		pthread_create(&t_client[i], NULL, client_tasks, NULL);
+   	}
+	
+	
+	/* initialize_threads();
 
 	do  {
 		//apagar!!!
 		//gets(linha);	
 
-		/* parametros(descritor socket, endereco da memoria, tamanho da memoria, flag) */
+		parametros(descritor socket, endereco da memoria, tamanho da memoria, flag)
 		//send(rem_sockfd, &linha, sizeof(linha), 0);
 		//solicitation = send_solicitation(rem_sockfd);
 		initialize_threads_solicitation();
 		
 
-		/* parametros(descritor socket, endereco da memoria, tamanho da memoria, flag) */ 
+		parametros(descritor socket, endereco da memoria, tamanho da memoria, flag)
 		//recv(rem_sockfd, &linha, sizeof(linha), 0);
 		initialize_threads_receiving();
 
 		//printf("Recebi %s\n", linha);
 	}while(strcmp(linha,"exit"));
-	/* fechamento do socket remota */ 	
-	close(rem_sockfd);
+	fechamento do socket remota
+	close(rem_sockfd); */
 }
 
 
