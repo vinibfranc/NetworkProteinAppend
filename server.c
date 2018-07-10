19887e73072d84552ac04acc1ccae82dc3e28ee2
Vinicius <vinibfranc@gmail.com>
2018-07-10 16:19:11 -0300

    Alterações no servidor

--- server.c
+++ server.c
@@ -8,8 +8,6 @@
 #include <pthread.h>
 #include <time.h>
 
-int loc_sockfd;
-
 /* struct que representa a mensagem */
 /* Estrutura: quantidade de aminoácidos, solicitação ou reposta e lista com aminoácidos solicitados */
 
@@ -51,44 +49,45 @@ typedef struct {
 }*/
 
 int randomize_aminoacid() {
-	int aminoacids_qtd = 22;
-	char aminoacids[] = {'A', 'R', 'N', 'D', 'C', 'E', 'Q', 'G', 'H', 'I', 'U', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W','Y', 'V', 'O'};
-	int j, num, result;	
-	srand(time(NULL));	
-	num = 0 + (rand() % 21);
-	printf("\nNumero sorteado: %d \n", num);
+	//srand(time(NULL));
+	int aminoacids_qtd = 20;
+	char aminoacids[] = {'A', 'R', 'N', 'D', 'C', 'E', 'Q', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W','Y', 'V'};
+	int j, num;	
+	num = 0 + (rand() % 20); 
+	printf("\nNumero sorteado: %d -> Aminiácido: %c\n", num, aminoacids[num]);
 		
-	for(j=0; j<aminoacids_qtd; j++) {
-		if(j == num) {
-			result = aminoacids[j];
+	/*for(j=0; j<aminoacids_qtd; j++) {
+		if(j == num) { // isso não é necessário, só usar aminoacids[num] e retornar
+			result = aminoacids[j]; // colocando um char em um int
 			printf("Sorteado: %d -> Aminoácido: %c", num, aminoacids[j]);
 			break;
 		}
-	}
-	return result;
+	}*/
+	return aminoacids[num]; 
 }
 
-int send_aminoacid(int quantity) {
+/*int send_aminoacid(int quantity) {
 
 	int i;
-	/* Inicializando mensagem */
+	Inicializando mensagem
 	aatp_msg m = { 0 };
-	/* Preenchendo dados */
-	m.method = 'R'; /* Resposta */
-	m.size = quantity; /* Enviar a quantidade que foi solicitada */
-	/* Zerando payload para evitar enviar lixo
-	   caso seja feita uma solicitação de menos de 5 aminoácidos */
+	Preenchendo dados
+	m.method = 'R'; Resposta
+	m.size = quantity;  Enviar a quantidade que foi solicitada
+	Zerando payload para evitar enviar lixo
+	   caso seja feita uma solicitação de menos de 5 aminoácidos
 	memset(&m.payload, 0, sizeof m.payload);
-	/* Preencher com os dados */
+	Preencher com os dados
 	for(i=0; i<quantity; i++){
 		int rand_n = randomize_aminoacid();
 		m.payload[i] = rand_n;
 	}
-	/* Enviando resposta */
+
+	Enviando resposta //Para enviar a resposta ele primeiro precisa criar o socket, ouvir o socket com listen() e accept(), olha os exemplos que ele deu nas atividades
 	printf("Enviando aminoácido");
 	int s = send(loc_sockfd, &m, sizeof(m), 0);
 	return s;
-}
+}*/
 
 
 /*==================================================
@@ -98,9 +97,11 @@ Implementação do servidor
 ====================================================*/
 
 int main(int argc, char *argv[]) {
+	srand(time(NULL));
 
 	/* Variaveis Locais */
-	int loc_newsockfd, tamanho;
+	int i;
+	int loc_sockfd, loc_newsockfd, tamanho;
 	char linha[81];		
 	/* Estrutura: familia + endereco IP + porta */
 	struct sockaddr_in loc_addr;
@@ -135,7 +136,7 @@ int main(int argc, char *argv[]) {
 	
 	/* parametros(descritor socket,
 	numeros de conexões em espera sem serem aceitos pelo accept)*/
-	listen(loc_sockfd, 5);
+	listen(loc_sockfd, 5); //Colocar este listen dentro de um while loop 
 	printf("> aguardando conexao\n");
 
 	tamanho = sizeof(struct sockaddr_in);
@@ -143,19 +144,42 @@ int main(int argc, char *argv[]) {
 	/* parametros(descritor socket, estrutura do endereco local, comprimento do endereco)*/
        	loc_newsockfd =	accept(loc_sockfd, (struct sockaddr *)&loc_addr, &tamanho);
 
-	do  {
+	while(1)  {
 
 		/* parametros(descritor socket, endereco da memoria, tamanho da memoria, flag) */
  		recv(loc_newsockfd, &linha, sizeof(linha), 0);
 		//printf("Recebi %s\n", linha);
-		printf("Recebi uma solicitação de aminoácidos");
+		printf("Recebi uma solicitação de aminoácidos!");
 
 		/* parametros(descritor socket, endereco da memoria, tamanho da memoria, flag) */ 
 		//send(loc_newsockfd, &linha, sizeof(linha), 0);
 		//printf("Renvia %s\n", linha);
-		send_aminoacid(1);
+		//send_aminoacid(1); // Aqui tu não ta enviando o AA pelo socket, tem que por esse valor retornado dentro de um send() ou write()
 
-	}while(strcmp(linha,"exit"));
+
+		/*========================
+		Envio do(s) aminoácido(s)
+		==========================*/
+
+		/* Inicializando mensagem */
+		aatp_msg m = { 0 };
+		/* Preenchendo dados */
+		m.method = 'R'; /* Resposta */
+		m.size = 3; /* TODO:=========Enviar a quantidade que foi solicitada =========*/
+		/* Zerando payload para evitar enviar lixo
+		   caso seja feita uma solicitação de menos de 5 aminoácidos */
+		memset(&m.payload, 0, sizeof m.payload);
+		/* Preencher com os dados */
+		for(i=0; i<3; i++){
+			int rand_n = randomize_aminoacid();
+			m.payload[i] = rand_n;
+		}
+
+		/* Enviando resposta */ //Para enviar a resposta ele primeiro precisa criar o socket, ouvir o socket com listen() e accept(), olha os exemplos que ele deu nas atividades
+		printf("Enviando aminoácido!");
+		send(loc_sockfd, &m, sizeof(m), 0);
+
+	}
 	/* fechamento do socket local */ 
 	close(loc_sockfd);
 	close(loc_newsockfd);
