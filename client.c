7e0be38cadd035382ed5bdc8b8c44d570ad79619
Vinicius Franceschi <vinibfranc@gmail.com>
2018-07-12 01:53:21 -0300

    Servidor e cliente funcionando :)

--- client.c
+++ client.c
@@ -1,3 +1,11 @@
+/*
+	Vinícius Franceschi
+	Informática Biomédica - UFCSPA
+	Redes de Computadores
+ */
+
+/* Cliente: reponsável pelo envio das solicitações e montagem da proteína */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,30 +14,23 @@
 #include <sys/time.h>
 #include <netinet/in.h>
 #include <pthread.h>
+#include <arpa/inet.h>
 #include <time.h>
 
-/* TODO: Alterar para quantidade de hosts (alunos) conforme quantos forem apresentar */
+// TODO: Alterar para quantidade de hosts (alunos) conforme quantos forem apresentar */
 #define CLIENTS 13
-//void *t_join_solicitation;
-//void *t_join_receiving;
-//pthread_t t_solicitation[CLIENTS];
-//pthread_t t_receiving[CLIENTS];
 void *t_client_join;
 pthread_t t_client[CLIENTS];
-pthread_mutex_t lock;
+//pthread_mutex_t lock;
 
-int rem_sockfd;
-int ips[CLIENTS];
-
-/* struct que representa a mensagem */
-/* Estrutura: quantidade de aminoácidos, solicitação ou reposta e lista com aminoácidos solicitados */
+//int ips[CLIENTS];
 
 /*==================================================
-
 Implementação do protocolo definido pela turma
-
 ====================================================*/
 
+// struct que representa a mensagem */
+// Estrutura: quantidade de aminoácidos, solicitação ou reposta e lista com aminoácidos solicitados */
 typedef struct {
     uint8_t size;
     char method;
@@ -39,138 +40,113 @@ typedef struct {
 /* Número aleatório de aminoácidos a serem solicitados */
 int random_quantity() {
 	int num;	
-	srand(time(NULL));
 	num = 1 + (rand() % 5);
 	return num;
 }
 
+// Será criado um cliente (thread) para cada aplicação dos colegas 
 void *client_tasks() {
 
 	/*========================
 	Implementação do cliente
 	==========================*/
 
-	/* ESTACAO REMOTA */
-	char *rem_hostname;
-	int rem_port;
-	int solicitation;
-	/* Estrutura: familia + endereco IP + porta */
-	struct sockaddr_in rem_addr;
-	char linha[81];
+	while(1) {
+		// Atributos do cliente
+		char *rem_hostname;
+		int rem_port;
+		int rem_sockfd;
+		// Estrutura: familia + endereco IP + porta
+		struct sockaddr_in rem_addr;
 
-	/*if (argc != 3) {
-		//printf("Parametros: <remote_port> \n");
-		printf("Parametros: <remote_host> <remote_port> \n");
-		exit(1);
-	}*/
+		/*if (argc != 3) {
+			//printf("Parametros: <remote_port> \n");
+			printf("Parametros: <remote_host> <remote_port> \n");
+			exit(1);
+		}*/
 
-	/* Construcao da estrutura do endereco local */
-	/* Preenchendo a estrutura socket loc_addr (família, IP, porta) */
-	
+		/*rem_hostname = argv[1];
+		rem_port = atoi(argv[2]);*/
 
-	//rem_hostname = ips[0];
+		// Construcao da estrutura do endereco local
+		// Preenchendo a estrutura socket loc_addr (família, IP, porta)
+		rem_hostname = "127.0.0.1";
+		rem_port = atoi("7004");
+		rem_addr.sin_family = AF_INET; // familia do protocolo
+		rem_addr.sin_addr.s_addr = inet_addr(rem_hostname); // endereco IP local 
+		rem_addr.sin_port = htons(rem_port); // porta local  
 
-	/*rem_hostname = argv[1];
-	rem_port = atoi(argv[2]);*/
-	rem_hostname = atoi("127.0.0.1");
-	rem_port = atoi("1337");
-	rem_addr.sin_family = AF_INET; /* familia do protocolo*/
-	rem_addr.sin_addr.s_addr = inet_addr(rem_hostname); /* endereco IP local */
-	rem_addr.sin_port = htons(rem_port); /* porta local  */
+		// Cria o socket para enviar e receber pacotes 
+		// parametros(familia, tipo, protocolo) 
+		rem_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+		
+		if (rem_sockfd < 0) {
+			perror("Criando stream socket");
+			exit(1);
+		}
+//printf("> Conectando ao servidor '%s:%d'\n", rem_hostname, rem_port);
 
-   	/* Cria o socket para enviar e receber datagramas */
-	/* parametros(familia, tipo, protocolo) */
-	rem_sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	
-	if (rem_sockfd < 0) {
-		perror("Criando stream socket");
-		exit(1);
+		// Estabelece uma conexão remota 
+		// parametros(descritor socket, estrutura do endereco remoto, comprimento do endereco) 
+		if (connect(rem_sockfd, (struct sockaddr *) &rem_addr, sizeof(rem_addr)) < 0) {
+			perror("Conectando stream socket");
+			exit(1);
+		}
+
+		/*========================
+		Envio da solicitação
+		==========================*/
+
+		int quantity = random_quantity();
+		printf("\n=================================\n");
+		printf("Estou solicitando | %d | aminoácidos\n", quantity);
+		printf("=================================\n\n");
+
+		//int z = 0;
+		//for(z = 0; z < quantity; z++) {
+			// Inicializando mensagem
+			aatp_msg m = { 0 };
+			// Preenchendo dados 
+			m.method = 'S'; // Solicitação
+			m.size = quantity;
+			// Zerando payload
+			memset(&m.payload, 0, sizeof m.payload);
+
+			// Enviando solicitação
+			send(rem_sockfd, &m, sizeof(m), 0);
+
+			/*========================
+			Recebimento da resposta
+			==========================*/
+
+			// Criando buffer 
+			aatp_msg recv_buffer;
+			size_t recv_buffer_len = sizeof(recv_buffer);
+			// Recebendo mensagem 
+			recv(rem_sockfd, &recv_buffer, recv_buffer_len, 0);
+			// Imprimindo resultados 
+
+			int q;
+			printf("\n\t====================================\n\n");
+			for(q=0; q < recv_buffer.size; q++) {
+				printf("\n\t Recebi --->  %c \n\n", recv_buffer.payload[q]);
+				//printf("\t== Method: %c ==\n", recv_buffer.method);
+				//printf("\t== Size: %d ==\n", recv_buffer.size);
+				//printf("\t== Payload: %c ==\n", recv_buffer.payload[q]);
+			}
+			printf("\t====================================\n");
+
+			//SALVAR EM ARRAY PARA COMPARAR COM A SEQUÊNCIA DA ALBUMINA
+		
+		//}
+		close(rem_sockfd);
 	}
-	printf("> Conectando no servidor '%s:%d'\n", rem_hostname, rem_port);
-
-   	/* Estabelece uma conexão remota */
-	/* parametros(descritor socket, estrutura do endereco remoto, comprimento do endereco) */
-	if (connect(rem_sockfd, (struct sockaddr *) &rem_addr, sizeof(rem_addr)) < 0) {
-		perror("Conectando stream socket");
-		exit(1);
-	}
-
-	/*========================
-	Envio da solicitação
-	==========================*/
-
-	int quantity = random_quantity();
-	
-	/* Inicializando mensagem */
-	aatp_msg m = { 0 };
-	/* Preenchendo dados */
-	m.method = 'S'; /* Solicitação */
-	//m.size = 1;
-	m.size = quantity;
-	/* Zerando payload */
-	memset(&m.payload, 0, sizeof m.payload);
-	/* Enviando solicitação */
-	send(rem_sockfd, &m, sizeof(m), 0);
-	printf("Enviando solicitação!!");
-
-	/*========================
-	Recebimento da resposta
-	==========================*/
-
-	/* Criando buffer */
-	aatp_msg recv_buffer;
-	size_t recv_buffer_len = sizeof(recv_buffer);
-	/* Recebendo mensagem */
-	recv(rem_sockfd, &recv_buffer, recv_buffer_len, 0);
-	/* Imprimindo resultados */
-	printf("\t Method: %c\n", recv_buffer.method);
-	printf("\t Size: %d\n", recv_buffer.size);
-	printf("\t Payload: %s\n", recv_buffer.payload);
 	
 }
 
-/* Função que será associada a uma thread para realizar múltiplas solicitações
-int *send_solicitation() {
-	pthread_mutex_lock(&lock);
-	
-	int quantity = random_quantity();
-	
-	Inicializando mensagem
-	aatp_msg m = { 0 };
-	Preenchendo dados
-	m.method = 'S'; Solicitação
-	//m.size = 1;
-	m.size = quantity;
-	Zerando payload
-	memset(&m.payload, 0, sizeof m.payload);
-	Enviando solicitação
-	int r = send(rem_sockfd, &m, sizeof(m), 0);
-	printf("Enviando solicitação!!");
-	pthread_mutex_unlock(&lock);
 
-	return r;
-}*/
-
-/* Função associada a uma thread para realizar recebimentos simultâneos
-void *receive_aminoacids() {
-	
-	pthread_mutex_lock(&lock);
-
-	Criando buffer
-	aatp_msg recv_buffer;
-	size_t recv_buffer_len = sizeof(recv_buffer);
-	Recebendo mensagem
-	recv(rem_sockfd, &recv_buffer, recv_buffer_len, 0);
-	Imprimindo resultados
-	printf("\t Method: %c\n", recv_buffer.method);
-	printf("\t Size: %d\n", recv_buffer.size);
-	printf("\t Payload: %s\n", recv_buffer.payload);
-
-	pthread_mutex_unlock(&lock);
-}*/
-
-/* Retorna a lista de IPs presentes no arquivo de configuração */
-int get_ips() {
+// Retorna a lista de IPs presentes no arquivo de configuração 
+/*int get_ips() {
 	FILE *config_file;
 	char each_line[2][15];
 	int i=0;
@@ -186,64 +162,25 @@ int get_ips() {
 	}
 }
 
-/*void initialize_threads_solicitation() {
-	int i;
-    	for(i=0; i<CLIENTS; i++) {
-       		pthread_create(&t_solicitation[i], NULL, send_solicitation, NULL);
-   	}
-    	for(i=0; i<CLIENTS; i++) {
-        	pthread_join(t_solicitation[i],&t_join_solicitation);
-	}
-}*/ 
-
-/*void initialize_threads_receiving() {
-	int i;
-    	for(i=0; i<CLIENTS; i++) {
-		pthread_create(&t_receiving[i], NULL, receive_aminoacids, NULL);
-   	}
-    	for(i=0; i<CLIENTS; i++) {
-		pthread_join(t_receiving[i],&t_join_receiving);
-	}
-}*/ 
-
 
 /*==================================================
-
 Implementação do cliente
-
 ====================================================*/
 
-/* Conexão do cliente com servidor e solicitação dos aminoácidos */
+// Conexão do cliente com servidor e solicitação dos aminoácidos 
 
-int main(int argc, char *argv[]) {
+//int main(int argc, char *argv[]) {
+int main() {
+
+	srand(time(NULL));	
 
 	int i;
-    	for(i=0; i<CLIENTS; i++) {
+    	for(i=0; i<1; i++) {
 		pthread_create(&t_client[i], NULL, client_tasks, NULL);
    	}
 	
-	
-	/* initialize_threads();
+	for(i=0; i<1; i++) {
+		pthread_join(t_client[i],&t_client_join);
+	}
 
-	do  {
-		//apagar!!!
-		//gets(linha);	
-
-		parametros(descritor socket, endereco da memoria, tamanho da memoria, flag)
-		//send(rem_sockfd, &linha, sizeof(linha), 0);
-		//solicitation = send_solicitation(rem_sockfd);
-		initialize_threads_solicitation();
-		
-
-		parametros(descritor socket, endereco da memoria, tamanho da memoria, flag)
-		//recv(rem_sockfd, &linha, sizeof(linha), 0);
-		initialize_threads_receiving();
-
-		//printf("Recebi %s\n", linha);
-	}while(strcmp(linha,"exit"));
-	fechamento do socket remota
-	close(rem_sockfd); */
 }
-
-
-
--- server.c
+++ server.c
@@ -1,3 +1,11 @@
+/*
+	Vinícius Franceschi
+	Informática Biomédica - UFCSPA
+	Redes de Computadores
+ */
+
+/* Servidor aguarda solicitações de aminoácidos dos clientes */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,13 +16,12 @@
 #include <pthread.h>
 #include <time.h>
 
-/* struct que representa a mensagem */
-/* Estrutura: quantidade de aminoácidos, solicitação ou reposta e lista com aminoácidos solicitados */
-
 /*==================================================
 Implementação do protocolo definido pela turma
 ====================================================*/
 
+// struct que representa a mensagem */
+// Estrutura: quantidade de aminoácidos, solicitação ou reposta e lista com aminoácidos solicitados */
 
 typedef struct {
     uint8_t size;
@@ -22,13 +29,14 @@ typedef struct {
     char payload[5];
 } aatp_msg;
 
+// Retorna um aminoácido aleatório para enviar ao cliente
 char randomize_aminoacid() {
-	//srand(time(NULL));
+
 	int aminoacids_qtd = 20;
 	char aminoacids[] = {'A', 'R', 'N', 'D', 'C', 'E', 'Q', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W','Y', 'V'};
 	int j, num;	
 	num = 0 + (rand() % 20); 
-	printf("\nNumero sorteado: %d -> Aminoácido: %c\n", num, aminoacids[num]);
+	//printf("\nNumero sorteado: %d -> Aminoácido: %c\n", num, aminoacids[num]);
 		
 	return aminoacids[num]; 
 }
@@ -39,14 +47,16 @@ Implementação do servidor
 ====================================================*/
 
 int main(int argc, char *argv[]) {
+
+	// Inicializando semente para números aleatórios
 	srand(time(NULL));
 
-	/* Variaveis Locais */
+	// Variaveis Locais 
 	int i;
 	int loc_sockfd, loc_newsockfd, tamanho;
 	
 
-	/* Estrutura: familia + endereco IP + porta */
+	// Estrutura: familia + endereco IP + porta 
 	struct sockaddr_in loc_addr;
 	
 	if (argc != 2) {
@@ -54,8 +64,8 @@ int main(int argc, char *argv[]) {
 		exit(1);
 	}
 
-   	/* Cria o socket para enviar e receber datagramas */
-	/* parametros(familia, tipo, protocolo) */
+   	// Cria o socket para enviar e receber pacotes
+	// parametros(familia, tipo, protocolo) 
 	loc_sockfd = socket(AF_INET, SOCK_STREAM, 0);	
 	
 	if (loc_sockfd < 0) {
@@ -63,70 +73,84 @@ int main(int argc, char *argv[]) {
 		exit(1);
 	}
 
-	/* Construcao da estrutura do endereco local */
-	/* Preenchendo a estrutura socket loc_addr (família, IP, porta) */
-	loc_addr.sin_family = AF_INET; /* familia do protocolo*/
-	loc_addr.sin_addr.s_addr = INADDR_ANY; /* endereco IP local */
-	loc_addr.sin_port = htons(atoi(argv[1])); /* porta local  */
+	// Construcao da estrutura do endereco local
+	// Preenchendo a estrutura socket loc_addr (família, IP, porta)
+	loc_addr.sin_family = AF_INET; // familia do protocolo
+	loc_addr.sin_addr.s_addr = INADDR_ANY; // endereco IP local 
+	loc_addr.sin_port = htons(atoi(argv[1])); // porta local 
 	bzero(&(loc_addr.sin_zero), 8);
 
-   	/* Bind para o endereco local*/
-	/* parametros(descritor socket, estrutura do endereco local, comprimento do endereco) */
+   	// Bind para o endereco local
+	// parametros(descritor socket, estrutura do endereco local, comprimento do endereco)
 	if (bind(loc_sockfd, (struct sockaddr *) &loc_addr, sizeof(struct sockaddr)) < 0) {
 		perror("Ligando stream socket");
 		exit(1);
 	}
 	
-	/* parametros(descritor socket,
-	numeros de conexões em espera sem serem aceitos pelo accept)*/
-	// ============> while em volta do listen() como tu mesmo disse
-
+	// parametros(descritor socket, numeros de conexões em espera sem serem aceitos pelo accept)
+	
+	// Loop infinito para espera de conexões
+	printf(">Esperando uma conexao\n");
 	while(1) {
 		listen(loc_sockfd, 5);
-		printf("> aguardando conexao\n");
 		
-		tamanho = sizeof(struct sockaddr_in); //PODE ESTAR AQUI
-		/* A mensagem recebida em bits é estruturada com o buffer do protocolo definido */		
+		int sock_size = sizeof(struct sockaddr_in);
+
+		/*===================================================================
+		Preparação da estrutura do protocolo para recebimento da solicitação
+		=====================================================================*/
+
+		// A mensagem recebida em bits é estruturada com o buffer do protocolo definido	
 		aatp_msg recv_buffer;
 
-	   	/* Accept permite aceitar um pedido de conexao, devolve um novo "socket" ja ligado ao emissor do pedido e o "socket" original*/
-		/* parametros(descritor socket, estrutura do endereco local, comprimento do endereco)*/
+	   	// Accept permite aceitar um pedido de conexao, devolve um novo "socket" ja ligado ao emissor do pedido e o "socket" original
+		// parametros(descritor socket, estrutura do endereco local, comprimento do endereco)
+		loc_newsockfd =	accept(loc_sockfd, (struct sockaddr *)&loc_addr, &sock_size);
 
-		loc_newsockfd =	accept(loc_sockfd, (struct sockaddr *)&loc_addr, &tamanho);
-
+		// Pega a quantidade de aminoácidos enviada
 		size_t buffer_len = sizeof(recv_buffer);
-		/* parametros(descritor socket, endereco da memoria, tamanho da memoria, flag) */
+		// parametros(descritor socket, endereco da memoria, tamanho da memoria, flag)
 		recv(loc_newsockfd, &recv_buffer, buffer_len, 0);
-		printf("Recebi uma solicitação de aminoácidos!");
+		printf("\n=================================\n");
+		printf("Recebendo solicitação de | %d | aminoácidos\n", recv_buffer.size);
+		printf("=================================\n\n");
 
-		/* Inicializando mensagem */
+		/*==================================================
+		Resposta com a quantidade de aminoácidos solicitados
+		====================================================*/
+
+		// Inicializando mensagem
 		aatp_msg m = { 0 };
-
-		/* Preenchendo dados */
-		m.method = 'R'; /* Resposta */
-		m.size = buffer_len; 
-		/* Zerando payload para evitar enviar lixo
-		caso seja feita uma solicitação de menos de 5 aminoácidos */
+		// Preenchendo dados
+		m.method = 'R'; // Resposta
+		m.size = recv_buffer.size; 
+		// Zerando payload para evitar enviar lixo caso seja feita uma solicitação de menos de 5 aminoácidos 
 		memset(&m.payload, 0, sizeof(m.payload));
 		
-		int num_aminos;
-		for(num_aminos=0; num_aminos<(int)m.size; num_aminos++) {
-			m.payload[num_aminos] = randomize_aminoacid();
+		printf("\n\t====================================\n");
+		// Itera de acordo com size recebido pelo cliente 
+		for(i=0; i<recv_buffer.size; i++){
+			char rand_n = randomize_aminoacid();
+			printf("\n\t Enviei --->  %c \n\n", rand_n);
+			m.payload[i] = rand_n;
 		}
+		printf("\t====================================\n");
 		
-		
-		/* parametros(descritor socket, endereco da memoria, tamanho da memoria, flag) */ 
+		// Envia a quantidade de aminoácidos solicitada pelo cliente
+		// parametros(descritor socket, endereco da memoria, tamanho da memoria, flag) 
 		send(loc_newsockfd, &m, sizeof(m), 0);
-		printf("Enviando aminoácidos!!");
-		//printf("Renvia %s\n", "ARXVA");
+//printf("\nEnviando aminoácidos\n!!");
+
+		sleep(2);
+
 		close(loc_newsockfd);
 			
 	}
 	
 	close(loc_sockfd);
+}
 
-
-
+	/* código antigo */
 
 	//listen(loc_sockfd, 1); 
 
@@ -185,4 +209,4 @@ int main(int argc, char *argv[]) {
 	//}
 	/* fechamento do socket local */ 
 	
-}
+
